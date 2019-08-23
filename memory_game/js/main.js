14ffafd5b09c7e841447fcbf6436f799835cd23b
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-08-22 17:06:58 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
