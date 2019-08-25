f5985f5978ce97eca44e4b8c952e539a79db337a
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-08-24 20:43:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,13 +33,16 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	//console.log("User flipped " + cards[cardId].rank);
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
 
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -47,10 +50,18 @@ var flipCard = function(cardId){
 
 }
 
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
 
-flipCard(0);
-flipCard(2);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
+createBoard();
 
 
 
